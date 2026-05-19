9bd0384b0fcfe05fe8a646ed7339f4fb47a37d68
Umut <2996934032@qq.com>
2026-05-19 08:26:26 +0800

    一些参数以及小修改

--- src/convert_pcd2_available_map/launch/convert_pcd2_octomap.launch.py
+++ src/convert_pcd2_available_map/launch/convert_pcd2_octomap.launch.py
@@ -8,7 +8,7 @@ from launch_ros.substitutions import FindPackageShare
 def generate_launch_description():
     pcd_launch_arg = DeclareLaunchArgument(
         'pcd',
-        default_value='merged_ground',
+        default_value='map',
         description='PCD文件名(不需要.pcd后缀,存放在convert_pcd2_available_map包的pcd目录下)'
     )
 
@@ -45,7 +45,7 @@ def generate_launch_description():
             {"prob_miss": 0.4},
             {"thres_min": 0.12},
             {"thres_max": 0.97},
-            {"hole_filling_radius": 2},
+            {"hole_filling_radius": 0},
         ],
     )
 
